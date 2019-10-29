03b1d3dbdf58cca131c30e46992d92f4e9585ea2
SAKI-toki <toki.ryuka.senriyama@gmail.com>
2019-10-29 16:18:37 +0900

    移動を実装

--- BubbleFightProject/Assets/Scripts/Players/PlayerController.cs
+++ BubbleFightProject/Assets/Scripts/Players/PlayerController.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+/// <summary>
+/// プレイヤーの制御クラス
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField, Tooltip("移動力")]
+    float movePower = 10.0f;
 
-    // Update is called once per frame
+    [SerializeField, Tooltip("プレイヤーの番号")]
+    int playerNumber = 0;
+    [SerializeField, Tooltip("自分自身のRigidbody")]
+    new Rigidbody rigidbody = null;
+
     void Update()
     {
-        
+        Move();
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    void Move()
+    {
+        float stickHorizontal = SwitchInput.GetHorizontal(playerNumber) * movePower;
+        float stickVertical = SwitchInput.GetVertical(playerNumber) * movePower;
+
+        rigidbody.AddForce(stickHorizontal, 0, stickVertical);
     }
 }
