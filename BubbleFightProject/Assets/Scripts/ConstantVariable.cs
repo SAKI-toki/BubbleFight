f51e699a1b6dcbb09ecea217130e8aad2987bf55
SAKI-toki <toki.ryuka.senriyama@gmail.com>
2019-11-01 12:19:19 +0900

    プレイヤー参加シーンのベースを作成

---
+++ BubbleFightProject/Assets/Scripts/ConstantVariable.cs
@@ -0,0 +1,8 @@
+/// <summary>
+/// プレイヤーの参加に関する情報
+/// </summary>
+public static class PlayerCount
+{
+    public const int MinValue = 2;
+    public const int MaxValue = 8;
+}
---
+++ BubbleFightProject/Assets/Scripts/PlayerJoin/PlayerJoinController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのゲームへの参加の制御
+/// </summary>
+public class PlayerJoinController : MonoBehaviour
+{
+    /// <summary>
+    /// 参加
+    /// </summary>
+    public void Join()
+    {
+        transform.localScale = Vector3.one * 2;
+    }
+
+    /// <summary>
+    /// 非参加
+    /// </summary>
+    public void UnJoin()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
