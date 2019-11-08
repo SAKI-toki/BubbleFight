bb153d4ac1d7f6a6ba720924c983522ceb8b555f
SAKI-toki <toki.ryuka.senriyama@gmail.com>
2019-11-08 21:36:21 +0900

    ファイル、フォルダ名の変更、一部ファイルの統合

--- BubbleFightProject/Assets/Scripts/PlayerJoin/PlayerJoinManager.cs
+++ BubbleFightProject/Assets/Scripts/PlayerJoin/PlayerJoinManager.cs
@@ -117,3 +117,12 @@ public class PlayerJoinManager : MonoBehaviour
     }
 #endif
 }
+
+/// <summary>
+/// プレイヤーの参加に関する情報
+/// </summary>
+public static class PlayerCount
+{
+    public const int MinValue = 2;
+    public const int MaxValue = 8;
+}
