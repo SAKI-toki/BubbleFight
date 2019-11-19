d32ede3b72d070f3cc9039dfe2b39acc7148e5d1
SAKI-toki <toki.ryuka.senriyama@gmail.com>
2019-11-19 10:02:35 +0900

    ポイント追加時のUIのアニメーションを追加

--- BubbleFightProject/Assets/Scripts/UI/AddPointUIGenerator.cs
+++ BubbleFightProject/Assets/Scripts/UI/AddPointUIGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// ポイントの追加のUIの生成クラス
+/// </summary>
 public class AddPointUIGenerator : MonoBehaviour
 {
     [SerializeField, Tooltip("+のUIリスト")]
@@ -7,8 +10,9 @@ public class AddPointUIGenerator : MonoBehaviour
     [SerializeField, Tooltip("-のUIリスト")]
     GameObject[] MinusUIList = null;
 
-
-
+    /// <summary>
+    /// 外部から呼び出す生成関数
+    /// </summary>
     public void AddPoint(int index, int point)
     {
         if (point > 0)
@@ -21,16 +25,25 @@ public class AddPointUIGenerator : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// プラスの呼び出し
+    /// </summary>
     void PlusPoint(int point, Camera renderCamera)
     {
         SetCamera(Instantiate(PlusUIList[point - 1]), renderCamera);
     }
 
+    /// <summary>
+    /// マイナスの呼び出し
+    /// </summary>
     void MinusPoint(int point, Camera renderCamera)
     {
         SetCamera(Instantiate(MinusUIList[Mathf.Abs(point) - 1]), renderCamera);
     }
 
+    /// <summary>
+    /// カメラのセット
+    /// </summary>
     void SetCamera(GameObject ui, Camera renderCamera)
     {
         var canvas = ui.GetComponent<Canvas>();
